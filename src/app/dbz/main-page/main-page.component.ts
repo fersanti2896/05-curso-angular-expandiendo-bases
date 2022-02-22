62b468c5a3e51d6f391560160255df588a79a006
Fernando Nicolas <fersanti2896@gmail.com>
2022-02-21 19:01:29 -0600

    Directiva ngModel en main-page.component

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Frezeer',
+    poder: 1000000
+  }
+
   agregar() {
-    console.log('Esto es una prueba!');
+    console.log(this.nuevo);
   }
 }
